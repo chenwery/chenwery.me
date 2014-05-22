8d64797a8e6744862e66263e96b316ed48450b24
CW Yeung <chenwery@gmail.com>
2014-05-22 20:03:30 +0800

    add delete & modify modules

--- blog/models/connection.js
+++ blog/models/connection.js
@@ -1,10 +1,19 @@
 /*
  * 链接数据库方法
  */
-
+'use strict';
 exports.exec = function (operation, callback) {
     var mysql = require('mysql');
-    var config = require('./db.json');//数据库配置
+    var config = require('../../../db.json');//数据库配置文件
+    /*
+        db.json:
+        {
+          "host": "数据库主机（ip或域名）",
+          "user": "用户名",
+          "password": "密码",
+          "database": "数据库"
+        }
+     */
     var con = mysql.createConnection(config);
 
     con.connect();
