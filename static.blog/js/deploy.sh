2173823a7df05072becf9825ba9823d2b5ae9be8
CW Yeung <chenwery@gmail.com>
2014-05-25 21:22:52 +0800

    use javascript module loader seajs

---
+++ static.blog/js/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+jsdir=/home/web/webdir/chenwery.me/static.blog/js
+
+cd ${jsdir}
+spm install
---
+++ static.blog/js/pages/index/src/index.js
@@ -0,0 +1,6 @@
+define(function (require, modules) {
+    'use strict';
+    var $ = require('jquery');
+    console.log($(document.body));
+    console.log(window.location.href);
+});
