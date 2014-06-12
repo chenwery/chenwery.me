da947b9b318453a6d683b1e1d6fe8ffc2611c4df
CW Yeung <chenwery@gmail.com>
2014-06-12 16:41:24 +0800

    change styles

--- static.blog/js/pages/post/src/post.js
+++ static.blog/js/pages/post/src/post.js
@@ -2,8 +2,6 @@ define(function (require, exports, modules) {
     'use strict';
     var $ = require('$');
     
-    $('pre').addClass('prettyprint').each(function (i, pre) {
-        var code = pre.firstChild.innerHTML;
-        pre.innerHTML = prettyPrintOne(code);
-    });
+    $('pre').addClass('prettyprint');
+    prettyPrint();
 });
